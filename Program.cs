2924600ccc523b0af2d792886886f7e83b48c708
Alejandra Noemi Nuñez Juarez <alejandra_njuarez@hotmail.com>
2024-11-02 22:13:46 -0300

    cambio ale main

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace ControlStock
             app.UseRouting();
 
             app.UseAuthorization();
-
+            Console.WriteLine("hola Alejandra");
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
